9d259c646ed82dfc911890128da3d51f5695e256
anthonypileggi <anthony@ifixit.com>
2018-10-19 10:16:14 -0400

    add more punctuation to remove from the word vector before inserting

--- R/prepare_words.R
+++ R/prepare_words.R
@@ -2,5 +2,5 @@
 #' Prepare a word(s)
 #' @param x word list (character/vector)
 prepare_words <- function(x) {
-  stringr::str_replace_all(toupper(x), " ", "")
+  stringr::str_replace_all(toupper(x), " |-|'|\\.", "")
 }

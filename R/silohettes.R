7ea06478956384f1ce0f2258d5f5098b802ab493
anthonypileggi <anthony@ifixit.com>
2018-11-12 10:28:31 -0500

    add links to animal silohette images

---
+++ R/silohettes.R
@@ -0,0 +1,22 @@
+
+#' Links to silohette images
+#' @export
+silohettes <- function() {
+  list(
+    wolf = "http://clipart-library.com/download/clipart/pT7KdGX8c.htm",
+    beaver = "http://clipart-library.com/download/clipart/pTqK86Xgc.htm",
+    deer = "http://clipart-library.com/download/clipart/ATbj8LAXc.htm",
+    bull = "http://clipart-library.com/download/clipart/kcKBeLB7i.htm",
+    elephant = "http://clipart-library.com/download/clipart/pT78y56Bc.htm",
+    bear = "http://clipart-library.com/download/clipart/8cxnLx99i.htm",
+    giraffe = "http://clipart-library.com/download/clipart/piqK8MEyT.htm",
+    bat = "http://clipart-library.com/download/clipart/ziXe8koXT.htm",
+    horse = "http://clipart-library.com/download/clipart/kTKBeLbbc.htm",
+    sheep = "http://clipart-library.com/download/clipart/pc78y5b9i.htm",
+    cat = "http://clipart-library.com/download/clipart/rcLnxK7Ri.htm",
+    frog = "http://clipart-library.com/download/clipart/6iyXKnbKT.htm",
+    rabbit = "http://clipart-library.com/download/clipart/E6ipdBLcE.htm",
+    dog = "http://clipart-library.com/download/clipart/k8Txzgyip.htm",
+    butterfly = "http://clipart-library.com/download/clipart/MdT9axXi7.htm"
+  )
+}

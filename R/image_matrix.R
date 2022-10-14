27e118fe7cb15bd9144345112a03cf0a96e0c917
Anthony Pileggi <apileggi20@gmail.com>
2022-10-14 16:41:39 -0400

    addparams -- use rows/columns instead of r/c

--- R/image_matrix.R
+++ R/image_matrix.R
@@ -1,16 +1,16 @@
-
 #' Convert an image to a 0/1 matrix
-#' @param img image path
-#' @param r number of rows (numeric/sclar)
+#' @param img full path to image (character/scalar)
+#' @param rows number of rows (numeric/scalar)
+#' @param columns number of columns (numeric/scalar)
 image_matrix <- function(img = "https://upload.wikimedia.org/wikipedia/commons/9/96/Tux_Paint_banana.svg",
-                          r = 10,
-                          c = 10) {
+                          rows = 10,
+                          columns = 10) {
 
   # load image
   img <- magick::image_read(img)
 
   # scale to size of word_grid
-  img <- magick::image_scale(img, paste0(r,"x", c, "!"))
+  img <- magick::image_scale(img, paste0(rows,"x", columns, "!"))
 
   # convert the image to a black/white matrix representation
   C <- purrr::map(1:dim(img[[1]])[1], ~img[[1]][.x, , ] == "ff")     # white = 'ff ff ff ff'
